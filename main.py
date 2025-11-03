71a8fa765296a9b56749f236933a73270bc0a127
olgaolga500 <olgaolga500@gmail.com>
2025-11-04 00:13:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ import os
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
 # Читаем JSON из переменной окружения
-service_account_info = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT"])
+import base64
+service_account_info = json.loads(base64.b64decode(os.environ["GOOGLE_SERVICE_ACCOUNT_B64"]).decode("utf-8"))
+
 
 creds = ServiceAccountCredentials.from_json_keyfile_dict(service_account_info, scope)
 
